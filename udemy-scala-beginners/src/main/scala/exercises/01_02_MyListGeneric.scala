2721a78a547ab6faf13631804da02c0f7d2ab27b
mkozi <mkozi.junk@gmail.com>
2022-11-27 13:20:23 -0600

    Generics exercise done

--- udemy-scala-beginners/src/main/scala/exercises/01_02_MyListGeneric.scala
+++ udemy-scala-beginners/src/main/scala/exercises/01_02_MyListGeneric.scala
@@ -1,39 +1,39 @@
 package exercises
 
-class MyListGeneric[T]:
+abstract class MyListGeneric[+T]:
   def head(): T
   def tail(): MyListGeneric[T]
   def isEmpty(): Boolean
-  def add(el: T): MyListGeneric[T]
+  def add[U >: T](el: U): MyListGeneric[U]
   def printElements(): String
   override def toString(): String = s"[${printElements()}]"
 
-class MyListGenericEmpty[T] extends MyListGeneric[T]:
-  def head(): T = throw new NoSuchElementException
-  def tail(): MyListGeneric[T] = throw new NoSuchElementException
+class MyListGenericEmpty[Nothing] extends MyListGeneric[Nothing]:
+  def head(): Nothing = throw new NoSuchElementException
+  def tail(): MyListGeneric[Nothing] = throw new NoSuchElementException
   def isEmpty(): Boolean = true
-  def add[T](n: T): MyListGenericNonEmpty[T] = MyListGenericNonEmpty[T](n, this)
+  def add[T >: Nothing](n: T): MyListGeneric[T] = MyListGenericNonEmpty(n, this)
   override def printElements(): String = ""
 
-class MyListGenericNonEmpty[T](val headVal: T = 0, val tailVal: MyListGeneric[T]  = null) extends MyListGeneric[T]:
+class MyListGenericNonEmpty[+T](val headVal: T, val tailVal: MyListGeneric[T]) extends MyListGeneric[T]:
   def head(): T = headVal
   def tail(): MyListGeneric[T] = tailVal
   def isEmpty(): Boolean = false
-  def add[T](n: T): MyListGenericNonEmpty[T] = MyListGenericNonEmpty[T](n, this)
+  def add[U >: T](n: U): MyListGeneric[U] = MyListGenericNonEmpty[U](n, this)
   override def printElements(): String = 
     this.isEmpty() match
       case true => ""
       case false => s"${this.tail().printElements()} ${this.head()}" 
 
 @main def myListGeneric() =
-  val mylist = MyListGenericEmpty[String]()
+  val mylist = MyListGenericEmpty()
   println(s"mylist: ${mylist.toString()}")
 
-  val mylist1 = mylist.add("11")
+  val mylist1 = mylist.add("111")
   println(s"mylist: ${mylist1.toString()}")
 
-  val mylist2 = mylist1.add("22")
+  val mylist2 = mylist1.add("222")
   println(s"mylist: ${mylist2.toString()}")
   
-  val mylist3 = MyListGenericNonEmpty[String]("3", MyListGenericNonEmpty("2", MyListGenericNonEmpty("1", MyListGenericEmpty())))
+  val mylist3 = MyListGenericNonEmpty[String]("!", MyListGenericNonEmpty("Scala", MyListGenericNonEmpty("Hello", MyListGenericEmpty())))
   println(s"mylist: ${mylist3.toString()}")
