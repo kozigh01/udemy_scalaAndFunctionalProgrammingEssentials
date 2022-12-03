38b53588e458f6893153c17f3c4ab0cb664d2ec9
mkozi <mkozi.junk@gmail.com>
2022-12-03 07:29:57 -0600

    Exercise 01_03

--- udemy-scala-beginners/src/main/scala/exercises/01_03_MyListExpanded.scala
+++ udemy-scala-beginners/src/main/scala/exercises/01_03_MyListExpanded.scala
@@ -15,15 +15,15 @@ abstract class MyList3[+T]:
   def add[U >: T](el: U): MyList3[U]
   def printElements(): String
   override def toString(): String = s"[${printElements()}]"
-  def map[U](transformer: MyTransformer[T, U]): MyList3[U]
+  def map[V >: T, U >: T](transformer: MyTransformer[V, U]): MyList3[U]
 
-class MyList3Empty[Nothing] extends MyList3[Nothing]:
+class MyList3Empty[T <: Nothing] extends MyList3[T]:
   def head(): Nothing = throw new NoSuchElementException
   def tail(): MyList3[Nothing] = throw new NoSuchElementException
   def isEmpty(): Boolean = true
-  def add[T >: Nothing](n: T): MyList3[T] = MyList3NonEmpty(n, this)
+  def add[U >: T](n: U): MyList3[U] = MyList3NonEmpty(n, this)
   override def printElements(): String = ""
-  def map[Nothing, U](transformer: MyTransformer[Nothing, U]): MyList3[U] = MyList3Empty()
+  def map[V >: Nothing, U >: T](transformer: MyTransformer[V, U]): MyList3[U] = MyList3Empty()
 
 class MyList3NonEmpty[+T](val headVal: T, val tailVal: MyList3[T]) extends MyList3[T]:
   def head(): T = headVal
@@ -34,7 +34,7 @@ class MyList3NonEmpty[+T](val headVal: T, val tailVal: MyList3[T]) extends MyLis
     this.isEmpty() match
       case true => ""
       case false => s"${this.tail().printElements()} ${this.head()}" 
-  def map[V <: T, U](transformer: MyTransformer[V, U]): MyList3[U] =
+  def map[V >: T, U >: T](transformer: MyTransformer[V, U]): MyList3[U] =
     this.isEmpty() match
       case true => MyList3Empty()
       case false =>
@@ -42,15 +42,27 @@ class MyList3NonEmpty[+T](val headVal: T, val tailVal: MyList3[T]) extends MyLis
         new MyList3NonEmpty(newHead, tail().map(transformer))
     
 
+class Animal(val name: String):
+  override def toString(): String = s"I am an Animal named '$name'"
+class Dog(name: String) extends  Animal(name):
+  override def toString(): String = s"I am a Dog named '$name'"
+class Cat(name: String) extends  Animal(name):
+  override def toString(): String = s"I am a Cat named '$name'"
+class Bird(name: String) extends  Animal(name):
+  override def toString(): String = s"I am a Bird named '$name'"
+
 @main def myList3() =
   val mylist = MyList3Empty()
   println(s"mylist: ${mylist.toString()}")
 
-  val mylist1 = mylist.add("111")
+  val cat = Cat("boots")
+  val mylist1 = mylist.add(cat)
   println(s"mylist: ${mylist1.toString()}")
 
-  val MyList3 = mylist1.add("222")
+  val dog = Dog("rusty")
+  val MyList3 = mylist1.add(dog)
   println(s"mylist: ${MyList3.toString()}")
   
+  val bird = Bird("tweety")
   val mylist3 = MyList3NonEmpty[String]("!", MyList3NonEmpty("Scala", MyList3NonEmpty("Hello", MyList3Empty())))
   println(s"mylist: ${mylist3.toString()}")
