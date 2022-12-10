d10b9511bccf527ddefed6d9889ecd5f93c8126f
mkozi <mkozi.junk@gmail.com>
2022-12-10 16:44:36 -0600

    HIghrer order function & curried functions

--- udemy-scala-beginners/src/main/scala/lectures/03_FP/03_03_HOFandCurriedFunctions.scala
+++ udemy-scala-beginners/src/main/scala/lectures/03_FP/03_03_HOFandCurriedFunctions.scala
@@ -26,4 +26,18 @@ object HOFandCurriedFunctions extends App {
   val increment10By5 = nTimesBetter(_ + 10, 5)
   println(s"increment10(10): ${increment10By5(5)}")
       
+  // curried functions
+  val superAdder: Int => Int => Int = x => y => x + y
+  println(s"superAdder(5)(6): ${superAdder(5)(6)}")
+  val adder5: Int => Int = superAdder(5)  // y => 3 + y
+  println(s"adder5(6): ${adder5(6)}")
+
+  // functions with multiple parameter lists
+  def curriedFormatter(c: String)(x: Double): String = c.format(x)
+  val standardFormat: (Double => String) = curriedFormatter("%4.2f")
+  val preciseFormat: Double => String = curriedFormatter("%10.8f")
+  println(s"%4.2f".format(Math.PI))
+  println(s"standardFormat(Math.PI): ${standardFormat(Math.PI)}")
+  println(s"preciseFormat(Math.PI): ${preciseFormat(Math.PI)}")
+
 }
